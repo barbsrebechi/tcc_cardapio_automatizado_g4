93bb813f66bf3c052054d1ea9f72d34374ea1634
barbsrebechi <119419470+barbsrebechi@users.noreply.github.com>
2023-10-18 15:26:02 -0300

    18-10

--- controllers/itens.js
+++ controllers/itens.js
@@ -4,7 +4,9 @@ const db = require('../database/connection');
 module.exports = {
     async listarItens(request, response) {
         try {
-            return response.status(200).json({confirma: 'Listar Itens'});
+            const sql = 'SELECT itn_id, itn_nome_item, itn_descricao, itn_imagem  FROM ITENS;';
+            const itens = await db.query(sql);
+            return response.status(200).json({confirma: itens[0]});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -4,7 +4,10 @@ const db = require('../database/connection');
 module.exports = {
     async listarPedidos(request, response) {
         try {
-            return response.status(200).json({confirma: 'Listar Pedidos'});
+            const sql = 'SELECT ped_id, usu_id, ped_dt_hr_pedido, ped_status, ped_observacoes FROM PEDIDOS;';
+            const pedidos = await db.query(sql);
+
+            return response.status(200).json({confirma: pedidos[0]});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
