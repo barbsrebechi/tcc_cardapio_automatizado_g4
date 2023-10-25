4e814f4024381de49e42f4999d03c64549725ade
barbsrebechi <119419470+barbsrebechi@users.noreply.github.com>
2023-10-25 13:26:57 -0300

    25-10 lab baixo

--- controllers/itens.js
+++ controllers/itens.js
@@ -4,9 +4,17 @@ const db = require('../database/connection');
 module.exports = {
     async listarItens(request, response) {
         try {
+            //Armazena instruções SQL na constante
             const sql = 'SELECT itn_id, itn_nome_item, itn_descricao, itn_imagem  FROM ITENS;';
+            // executa instruções SQL e armazena o resultado na variavél itens
             const itens = await db.query(sql);
-            return response.status(200).json({confirma: itens[0]});
+            //armazena na constante o número de registro que a instrução SELECT retornou
+            const nReg = itens[0].length; 
+            return response.status(200).json(
+                {confirma: 'Sucesso',
+                 message:'Itens Cadastrados',
+                 nItens:nReg,
+                 itens: itens[0]});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
