efb09e63c3921858b514d472e65adb0b62e89b83
juliabarissa <anajuliabarissa@gmail.com>
2023-10-25 15:16:56 -0300

    25-10 cima

--- controllers/recargas.js
+++ controllers/recargas.js
@@ -1,47 +1,48 @@
-const { json } = require('express'); 
-const db = require('../database/connection'); 
+const { json } = require('express');
+const db = require('../database/connection');
 
 module.exports = {
     async listarRecargas(request, response) {
         try {
-            const sql ='SELECT rec_id, usu_id, rec_descricao, rec_valor, rec_data, ped_id FROM RECARGAS;';
+            const sql = 'SELECT rec_id, usu_id, rec_descricao, rec_valor, rec_data, ped_id FROM RECARGAS;';
             const recargas = await db.query(sql);
-            const nReg = recargas[0].lenght;
+            const nReg = recargas[0].length;
             return response.status(200).json(
                 {
-                    confirma: 'Sucesso', 
-                    message: 'Cadastro de usuário efetuado.',
-                    nItens: nReg, 
-                    itens: recargas[0]
-            }
-             );
-} catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+                    confirma: 'Sucesso',
+                    message: 'Recargas cadastradas',
+                    rec_id
+                    //nItens: nReg,
+                   // itens: recargas[0]
+                }
+            );
+        } catch (error) {
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
-  
+    },
+
     async cadatrarRecargas(request, response) {
         try {
-            return response.status(200).json({confirma: 'Cadastrar Recargas'});
+            return response.status(200).json({ confirma: 'Cadastrar Recargas' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
+    },
 
 
     async editarRecargas(request, response) {
         try {
-            return response.status(200).json({confirma: 'EditarRecargas'});
+            return response.status(200).json({ confirma: 'EditarRecargas' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
-  
+    },
+
     async apagarRecargas(request, response) {
         try {
-            return response.status(200).json({confirma: 'Apagar Recargas'});
+            return response.status(200).json({ confirma: 'Apagar Recargas' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
+    },
 };  
