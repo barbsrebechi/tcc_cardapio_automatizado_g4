7c5708bebfc079dfc521e8c40ffc4b2308475edb
FelipeSizilio <135241083+FelipeSizilio@users.noreply.github.com>
2023-10-26 09:29:07 -0300

    26/10

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,41 +1,46 @@
-const { json } = require('express'); 
-const db = require('../database/connection'); 
+const { json } = require('express');
+const db = require('../database/connection');
 
 
 module.exports = {
     async listarUsuarios(request, response) {
         try {
             const sql = 'SELECT usu_id, usu_nome, usu_cpf, usu_cod_cartao FROM USUARIOS;';
-           const  usuarios = await db.query(sql);
-            return response.status(200).json({confirma: usuarios[0]});
+            const usuarios = await db.query(sql);
+            const nReg = usuarios[0].length;
+            return response.status(200).json({ 
+                confirma: 'Sucesso', 
+                'nItens': nReg, 
+                'itens': usuarios[0] 
+            });
 
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
-  
+    },
+
     async cadatrarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'Cadastrar Usuarios'});
+            return response.status(200).json({ confirma: 'Cadastrar Usuarios' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
+    },
 
 
     async editarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'EditarUsuarios'});
+            return response.status(200).json({ confirma: 'EditarUsuarios' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
-  
+    },
+
     async apagarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'Apagar Usuarios'});
+            return response.status(200).json({ confirma: 'Apagar Usuarios' });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
+            return response.status(500).json({ confirma: 'Erro', message: error });
         }
-    }, 
+    },
 };  
